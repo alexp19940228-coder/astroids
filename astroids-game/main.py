d33ab750bd7bcadf25a1f5cbe0d36c5855f48f8f
Alexander Petersson <alexp.19940228@gmail.com>
2026-01-06 07:58:23 +0100

    .gitignore and logs

--- astroids-game/main.py
+++ astroids-game/main.py
@@ -1,8 +1,10 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
 
 def main():
     print("Starting Asteroids with pygame version: VERSION 2.6.1")
-
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
