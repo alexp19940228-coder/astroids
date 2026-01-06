0f55593d17234124918ddde478bb5ac227b4a95d
Alexander Petersson <alexp.19940228@gmail.com>
2026-01-06 08:09:55 +0100

    Made a black screen in a infiate loop.

--- astroids-game/main.py
+++ astroids-game/main.py
@@ -1,10 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from logger import log_state
 
 def main():
     print("Starting Asteroids with pygame version: VERSION 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
